df14caf50f254fddfa15755701d4001abe640aa2
Najib Jodiansyah <najibjodiansyah@Najibs-MacBook-Air.local>
2022-11-27 18:59:21 +0700

    adding hero section

--- src/scenes/HomePage.js
+++ src/scenes/HomePage.js
@@ -1,9 +1,52 @@
 import React from "react";
-import Button from 'elements/Button';
+import Section from 'elements/Section';
 import Header from 'parts/Header';
+import HeroImage from 'assets/images/hero-image.png'; 
+import Fade from 'react-awesome-reveal'
 
 export default function HomePage() {
     return (
-        <Header></Header>
+        <div className="body-wrap">
+             <Header></Header>
+             <main className="site-content">
+                <Section className="hero illustration-section-01" isCenterContent>
+                    <div className="container-sm">
+                        <div className="hero-inner section-inner">
+                            <div className="hero-content">
+                                <Fade bottom delay={100}>
+                                <h1 className="mt-0 mb-16">
+                                    Landing template for startups
+                                </h1>
+                                </Fade>
+                                <div className="container-xs">
+                                    <Fade bottom delay={500}>
+                                    <p
+                                        className="mt-0 mb-32"
+                                    >
+                                        Our landing page template works on all
+                                        devices, so you only have to set it up
+                                        once, and get beautiful results forever.
+                                    </p>
+                                    </Fade>
+                                </div>
+                            </div>
+                            <div
+                                className="hero-figure illustration-element-01"
+                            >
+                                <Fade bottom delay={1000}>
+                                <img
+                                    className="has-shadow"
+                                    src={HeroImage}
+                                    alt="Hero"
+                                    width="896"
+                                    height="504"
+                                />
+                                </Fade>
+                            </div>
+                        </div>
+                    </div>
+                </Section>
+            </main>
+        </div> 
     );
 }
