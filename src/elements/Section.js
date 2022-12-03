f49d29d23de00da0e529ad9acfefe4133f8fcb4f
Najib Jodiansyah <najibjodiansyah@Najibs-MacBook-Air.local>
2022-12-03 21:42:32 +0700

    adding feature section

--- src/elements/Section.js
+++ src/elements/Section.js
@@ -1,17 +1,14 @@
-import React from 'react'
-import propTypes from 'prop-types'
+import React from "react";
+import propTypes from "prop-types";
 
 export default function Section(props) {
-const className = ['section'];
-className.push(props.className);
-if ( props.isCenterContent) className.push('center-content');
-  return (
-    <section className={className.join(" ")}>{props.children}</section>
-  )
+  const className = ["section"];
+  className.push(props.className);
+  if (props.isCenterContent) className.push("center-content");
+  return <section className={className.join(" ")}>{props.children}</section>;
 }
 
 Section.propTypes = {
-    className: propTypes.string,
-    isCenterContent: propTypes.bool
-}
-
+  className: propTypes.string,
+  isCenterContent: propTypes.bool,
+};
